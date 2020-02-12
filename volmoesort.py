8aeb89a6319e0563dbde375f5a27ad418dfe77fb
tinganhsu <tinganhsu@gmail.com>
2020-02-12 20:20:24 +0800

    Update volmoesort.py
    cover.jpg to 00.jpg for cover as fisrt pic

--- volmoesort.py
+++ volmoesort.py
@@ -8,11 +8,11 @@ DIR = 'image/'
 total = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
 #扣掉cover和madeby這兩張
 total = total-2
-print(total)
+#print(total)
 for i in range(1,total+1):
  filename = 'html/'+str(i)+'.html'
  fp = open(filename, 'r', encoding = 'utf8')
- print(filename)
+ #print(filename)
  html = fp.readlines()
  #利用正規表現式在html中尋找jpg的位置
  imgRe = re.compile(r'src="(.+?\.jpg)"')
@@ -22,9 +22,9 @@ for i in range(1,total+1):
  #last = last[2:]
  #加上路徑以免跑掉
  newfilename = '/'+DIR+str(i)+'.jpg'
- print(last)
- print(newfilename)
+ #print(last)
+ #print(newfilename)
  os.rename(path+last, path+newfilename)
  fp.close()
  #print(fp.readlines())
- 
+os.rename(path+'/cover.jpg', path+'/00.jpg')
